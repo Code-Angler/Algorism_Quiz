150dd5cc29abc0ece5afb27bf1b7af1f8f48a3bc
Code_Angler <xkqkrtk@naver.com>
2021-05-09 00:11:52 +0900

    make function (check) and change name of Variable(변수)

--- lotto_algo.py
+++ lotto_algo.py
@@ -2,28 +2,46 @@
 
 from random import randint
 
-
 def generate_number(n):  # n개의 무작위한 번호를 추출하기 위한 함수
-    lucky_nums = []
+    numbers = []
 
-    while len(lucky_nums) < n:
-        lucky = randint(1, 45)  # 1에서 45사이의 정수를 랜덤하게 뽑음
-        if lucky not in lucky_nums:
-            lucky_nums.append(lucky)
+    while len(numbers) < n:
+        num = randint(1, 45)  # 1에서 45사이의 정수를 랜덤하게 뽑음
+        if num not in numbers:
+            numbers.append(num)
 
-    return lucky_nums
-
+    return numbers
 
 def draw_winning_numbers():  # 최종 당첨번호 7개 (6개의 무작위 넘버와 보너스번호)
     winning_nums = generate_number(7)
     return sorted(winning_nums[:6]) + winning_nums[6:]
 
-def count_matching_numbers(list_1, list_2):
+def count_matching_numbers(numbers, winning_numbers):
     count = 0
-    for num in list_1:
-        if num in list_2:
+    for list_num in numbers:
+        if list_num in winning_numbers:
             count += 1
 
     return count
 
-print(count_matching_numbers([2, 7, 11, 14, 25, 40], [2, 11, 13, 14, 30, 35]))
+
+def check(numbers, winning_numbers):
+    count = count_matching_numbers(numbers, winning_numbers[:6])
+    bonus_count = count_matching_numbers(numbers, winning_numbers[6:])
+
+    if count == 6:
+        return '1000000000'
+    elif count == 5 and bonus_count == 1:
+        return '50000000'
+    elif count == 5:
+        return '1000000'
+    elif count == 4:
+        return '50000'
+    elif count == 3:
+        return '5000'
+    else:
+        return '0'
+
+# 테스트
+print(check([2, 4, 11, 14, 25, 40], [4, 12, 14, 28, 40, 41, 6]))
+print(check([2, 4, 11, 14, 25, 40], [2, 4, 10, 11, 14, 40, 25]))
