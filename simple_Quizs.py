35e4ad422d84116b6a70f6ffda1dc684c0d4d09f
Code_Angler <xkqkrtk@naver.com>
2021-05-09 19:15:53 +0900

    Create "Hate same number" (By Python algolism)

--- simple_Quizs.py
+++ simple_Quizs.py
@@ -22,23 +22,23 @@ print("두 정수의 합은 {}입니다!!!".format(solution(first_num, sec_num))
 
 
 # 두 정수의 합에서 나올수있는 값을 모두 정리한 리스트(오답)
-#
-# def solution_01(numbers_01):
-#     answer_01 = []
-#     for num_01 in numbers_01:
-#         for num_02 in numbers_01:
-#             if num_01 != num_02:
-#                 sum_nums = num_01 + num_02
-#                 if sum_nums not in answer_01 :
-#                     answer_01.append(sum_nums)
-#     return sorted(answer_01)
-#
-#
-# print(solution_01([2,1,3,4,1])) # 4+4인 8도출력, 리스트에 서로 다른 요소만 더하는것이 목표여서 틀린답
 
+def solution_01_1(numbers_01_1):
+    answer_01_1 = []
+    for num_01_1 in numbers_01_1:
+        for num_02_1 in numbers_01_1:
+            if num_01_1 != num_02_1:
+                sum_nums_1 = num_01_1 + num_02_1
+                if sum_nums_1 not in answer_01_1:
+                    answer_01_1.append(sum_nums_1)
+    return sorted(answer_01_1)
+
+
+print(solution_01_1([2, 1, 3, 4, 1]))  # 1+1이 다른요소임에도 불구 하고 2가 출력이 안됨, 틀린답
 
 
 # 두 정수의 합에서 나올수있는 값을 모두 정리한 리스트(정답)
+
 
 def solution_01(numbers_01):
     answer_01 = []
@@ -50,5 +50,36 @@ def solution_01(numbers_01):
                     answer_01.append(sum_nums)
     return sorted(answer_01)
 
-print(solution_01([2,1,3,4,1])) #result [2,3,4,5,6,7]
-print(solution_01([5,0,2,7])) #result [2,5,7,9,12]
+
+print(solution_01([2, 1, 3, 4, 1]))  # result [2,3,4,5,6,7]
+print(solution_01([5, 0, 2, 7]))  # result [2,5,7,9,12]
+
+
+# 같은숫자는 싫어(오답: 반복되는 숫자가 뒤쪽에 다시나올떄는 출력해야하는데,,, 이 코드는 안됨)
+
+def solution_02_1(arr):
+    answer_02_1 = []
+    for factor_1 in arr:
+        if factor_1 not in answer_02_1:
+            answer_02_1.append(factor_1)
+    return answer_02_1
+
+
+print(solution_02_1([1, 1, 3, 3, 0, 1, 1]))  # result [1, 3, 0] but i want to get [1,3,0,1]
+
+
+# 같은숫자는 싫어 (정답)
+
+def solution_02(arr):
+    answer_02 = []
+    for factor in arr:
+        if factor not in answer_02:
+            answer_02.append(factor)
+        elif factor != answer_02[-1]:
+            answer_02.append(factor)
+    return answer_02
+
+
+print(solution_02([1, 1, 3, 3, 0, 1, 1]))  # result [1, 3, 0, 1]
+print(solution_02([4, 4, 4, 3, 3]))  # result [4, 3]
+print(solution_02([4, 7, 7, 4, 4, 4, 3, 3, 5, 3]))  # result [4, 7, 4, 3, 5, 3]
