59fbe404818ce383e73ba77b5d9a519323f742c7
Code_Angler <xkqkrtk@naver.com>
2021-05-09 17:11:30 +0900

    Create List (sum of two factor in origin_list) and sorted (By Python algolism)

--- simple_Quizs.py
+++ simple_Quizs.py
@@ -19,3 +19,36 @@ def solution(a, b):
 first_num = int(input("정수를 입력하세요 :"))
 sec_num = int(input("또 정수를 입력하세요 :"))
 print("두 정수의 합은 {}입니다!!!".format(solution(first_num, sec_num)))
+
+
+# 두 정수의 합에서 나올수있는 값을 모두 정리한 리스트(오답)
+#
+# def solution_01(numbers_01):
+#     answer_01 = []
+#     for num_01 in numbers_01:
+#         for num_02 in numbers_01:
+#             if num_01 != num_02:
+#                 sum_nums = num_01 + num_02
+#                 if sum_nums not in answer_01 :
+#                     answer_01.append(sum_nums)
+#     return sorted(answer_01)
+#
+#
+# print(solution_01([2,1,3,4,1])) # 4+4인 8도출력, 리스트에 서로 다른 요소만 더하는것이 목표여서 틀린답
+
+
+
+# 두 정수의 합에서 나올수있는 값을 모두 정리한 리스트(정답)
+
+def solution_01(numbers_01):
+    answer_01 = []
+    for i in range(len(numbers_01)):
+        for j in range(len(numbers_01)):
+            if i != j:
+                sum_nums = numbers_01[i] + numbers_01[j]
+                if sum_nums not in answer_01:
+                    answer_01.append(sum_nums)
+    return sorted(answer_01)
+
+print(solution_01([2,1,3,4,1])) #result [2,3,4,5,6,7]
+print(solution_01([5,0,2,7])) #result [2,5,7,9,12]
