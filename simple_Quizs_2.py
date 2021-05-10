d6f93b5aa6e472e43ea32026617ba6a8b36a71ed
Code_Angler <xkqkrtk@naver.com>
2021-05-11 00:41:42 +0900

    Create Algorism (make rectangle by *) 4 way

--- simple_Quizs_2.py
+++ simple_Quizs_2.py
@@ -11,3 +11,49 @@ def solution(num):
     else:
         return 'Even'
     return answer
+
+
+# 직사각형 별찍기
+
+# 문제 설명
+# 이 문제에는 표준 입력으로 두 개의 정수 n과 m이 주어집니다.
+# 별(*) 문자를 이용해 가로의 길이가 n, 세로의 길이가 m인 직사각형 형태를 출력해보세요.
+
+
+# 내가 푼 풀이
+
+def solution(a,b):
+    rec = ''
+    width = '*' * a
+    rectangle = (width + '\n') * b
+
+    rec = rec + rectangle
+
+    return rec
+
+print(solution(2, 3))
+
+
+# map을 이욯한 풀이법(풀이 참조-1)
+
+a, b = map(int, input().strip().split(' '))
+width = '*' * a
+rectangle = (width + '\n') * b
+print(rectangle)
+
+
+# map을 이용한 풀이법(플이 참조 -2)
+a, b = map(int, input().strip().split(' '))
+
+for i in range(b):
+    for j in range(a):
+        print('*',end='')
+    print('')
+
+# map을 이용한 풀이법(플이 참조 -3)
+
+a, b = map(int, input().strip().split(' '))
+for i in range(b):
+    print("*"*a)
+
+
