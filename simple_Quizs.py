00e5c6409b9d20a8ed435122eab5d29ca094bfe0
Code_Angler <xkqkrtk@naver.com>
2021-05-11 20:08:51 +0900

    Change name of variable (make simple)

--- simple_Quizs.py
+++ simple_Quizs.py
@@ -23,61 +23,61 @@ print("두 정수의 합은 {}입니다!!!".format(solution(first_num, sec_num))
 
 # 두 정수의 합에서 나올수있는 값을 모두 정리한 리스트(오답)
 
-def solution_01_1(numbers_01_1):
-    answer_01_1 = []
-    for num_01_1 in numbers_01_1:
-        for num_02_1 in numbers_01_1:
-            if num_01_1 != num_02_1:
-                sum_nums_1 = num_01_1 + num_02_1
-                if sum_nums_1 not in answer_01_1:
-                    answer_01_1.append(sum_nums_1)
-    return sorted(answer_01_1)
+def solution_1(numbers):
+    answer = []
+    for num_01 in numbers:
+        for num_02 in numbers:
+            if num_01 != num_02:
+                sum_nums = num_01 + num_02
+                if sum_nums not in answer:
+                    answer.append(sum_nums)
+    return sorted(answer)
 
 
-print(solution_01_1([2, 1, 3, 4, 1]))  # result [3, 4, 5, 6, 7], 1+1이 다른요소임에도 불구 하고 2가 출력이 안됨, 틀린답
+print(solution_1([2, 1, 3, 4, 1]))  # result [3, 4, 5, 6, 7], 1+1이 다른요소임에도 불구 하고 2가 출력이 안됨, 틀린답
 
 
 # 두 정수의 합에서 나올수있는 값을 모두 정리한 리스트(정답)
 
 
-def solution_01(numbers_01):
-    answer_01 = []
-    for i in range(len(numbers_01)):
-        for j in range(len(numbers_01)):
+def solution_2(numbers):
+    answer = []
+    for i in range(len(numbers)):
+        for j in range(len(numbers)):
             if i != j:
-                sum_nums = numbers_01[i] + numbers_01[j]
-                if sum_nums not in answer_01:
-                    answer_01.append(sum_nums)
-    return sorted(answer_01)
+                sum_nums = numbers[i] + numbers[j]
+                if sum_nums not in answer:
+                    answer.append(sum_nums)
+    return sorted(answer)
 
 
-print(solution_01([2, 1, 3, 4, 1]))  # result [2,3,4,5,6,7]
-print(solution_01([5, 0, 2, 7]))  # result [2,5,7,9,12]
+print(solution_2([2, 1, 3, 4, 1]))  # result [2,3,4,5,6,7]
+print(solution_2([5, 0, 2, 7]))  # result [2,5,7,9,12]
 
 
 # 같은숫자는 싫어(오답: 반복되는 숫자가 뒤쪽에 다시나올떄는 출력해야하는데,,, 이 코드는 안됨)
 
-def solution_02_1(arr):
-    answer_02_1 = []
-    for factor_1 in arr:
-        if factor_1 not in answer_02_1:
-            answer_02_1.append(factor_1)
-    return answer_02_1
+def solution_01(arr):
+    answer = []
+    for factor in arr:
+        if factor not in answer:
+            answer.append(factor)
+    return answer
 
 
-print(solution_02_1([1, 1, 3, 3, 0, 1, 1]))  # result [1, 3, 0] but i want to get [1,3,0,1]
+print(solution_01([1, 1, 3, 3, 0, 1, 1]))  # result [1, 3, 0] but i want to get [1,3,0,1]
 
 
 # 같은숫자는 싫어 (정답)
 
 def solution_02(arr):
-    answer_02 = []
+    answer = []
     for factor in arr:
-        if factor not in answer_02:
-            answer_02.append(factor)
-        elif factor != answer_02[-1]:
-            answer_02.append(factor)
-    return answer_02
+        if factor not in answer:
+            answer.append(factor)
+        elif factor != answer[-1]:
+            answer.append(factor)
+    return answer
 
 
 print(solution_02([1, 1, 3, 3, 0, 1, 1]))  # result [1, 3, 0, 1]
